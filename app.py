a003f621a140b5f4fb8a2f884a7fe44cc23c83ad
Sean <64774860+seany-web@users.noreply.github.com>
2020-05-12 21:47:54 +1200

    wrote function to clean data
    function cleans the data by converting the height into an int and the experience value to a boolean. Code has been tested

--- app.py
+++ app.py
@@ -1,8 +1,26 @@
 import constants
 
-def clean_data():
-    pass
+DIRTY_PLAYER_DATA = constants.PLAYERS
+cleaned_player_data = []
 
+def clean_data():
+    for player in DIRTY_PLAYER_DATA:
+        cleaned_player = {
+            'name': player['name'],
+            'guardians': player['guardians']
+        }
+        if player['experience'] == 'YES':
+            cleaned_player['experience'] = True
+        else:
+            cleaned_player['experience'] = False
+        cleaned_player['height'] = int(player['height'].split(' ')[0])
+        cleaned_player_data.append(cleaned_player)
 
 def balance_teams():
     pass
+
+# if __name__ == "__main__":
+#     clean_data()
+
+#     for player in cleaned_player_data:
+#         print(f"Player Name: {player['name']}\nGuardian(s): {player['guardians']}\nExperienced? {player['experience']}\nHeight: {player['height']} inches")
